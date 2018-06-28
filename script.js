59a302b775f6efc4607985207e6872f9fbe1c4c8
kvatoferma <jimmaggot@mail.ru>
2018-06-28 16:56:54 +0300

    Create scroll( ) function

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
 $(document).ready(() => {
+  var selectors = ['.one','.two','.three','.four'];
+  for (var i = 0; i < selectors.length; i++) {
+    scroll(selectors[i]);
+  }
 
-  $('a.one').click(function(e) {
-    $(document).scrollTop($('div.one').position()["top"]);
-    e.preventDefault();
-  });
-
-  $('a.two').click(function(e) {
-    $(document).scrollTop($('div.two').position()["top"]);
-    e.preventDefault();
-  });
-
-  $('a.three').click(function(e) {
-    $(document).scrollTop($('div.three').position()["top"]);
-    e.preventDefault();
-  });
-
-  $('a.four').click(function(e) {
-    $(document).scrollTop($('div.four').position()["top"]);
-    e.preventDefault();
-  });
+  function scroll(selector) {
+    $('a' + selector).click(function(e) {
+      $(document).scrollTop($('div' + selector).position()["top"]);
+      e.preventDefault();
+    });
+  }
 });
